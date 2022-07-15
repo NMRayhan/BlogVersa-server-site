2e03d682d7a8e57bd665a066de4084e31ca32fec
nmrayhan <nurmdrayhan97@gmail.com>
2022-07-15 08:26:52 -0700

    heroku

--- index.js
+++ index.js
@@ -12,10 +12,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
 async function run() {
     const BlogCollections = client.db("BlogVersa").collection("Blogs");
     const commentCollection = client.db("BlogVersa").collection("Comment");
